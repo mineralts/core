34c0c4b340491d87bc9f7f87d6792edcb38f4186
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-24 13:16:10 +0100

    :construction: Work in progress

---
+++ src/commands/Help.ts
@@ -0,0 +1,82 @@
+/*
+ * packages/GenerateManifest.ts
+ *
+ * (c) Parmantier Baptiste
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+import path from 'path'
+import { Command } from '../forge/entities/Command'
+
+export default class GenerateManifest extends Command {
+  public static commandName = 'help'
+  public static description = 'Generate manifest file'
+
+  public static settings = {
+    loadApp: true
+  }
+
+  public async run (): Promise<void> {
+    console.info()
+    const location = path.join(process.cwd(), 'forge-manifest.json')
+    const manifest = await import(location)
+
+    const maxWidth = Math.max.apply(
+      Math,
+      [...manifest.commands.map((command) => command.commandName.length)]
+    )
+
+    const sortedCommands = this.sortAndGroupCommands(manifest.commands)
+
+    sortedCommands
+      .forEach((container) => {
+        if (container.group === 'root') {
+          console.log('\x1B[34mAvailable commands\x1B[0m')
+        } else {
+          console.log(`\n\x1B[34m${container.group}\x1B[0m`)
+        }
+
+        container.commands.forEach(({ commandName, description }) => {
+          const whiteSpace = ''.padEnd(maxWidth - commandName.length, ' ')
+
+          console.log(
+            `  ${this.logger.colors.cyan(commandName)} ${whiteSpace}  ${this.logger.colors.dim(description)}`
+          )
+        })
+      })
+    console.log('')
+  }
+
+  private sortAndGroupCommands(commands) {
+    const groupsLiteral = commands.reduce((result, command) => {
+      const tokens = command.commandName.split(':')
+
+      const group = tokens.length > 1 ? tokens.shift()! : 'root'
+
+      result[group] = result[group] || []
+      result[group].push(command)
+
+      return result
+    }, {} as { [key: string]: any[] })
+
+    return Object.keys(groupsLiteral)
+      .sort((prev, curr) => {
+        if (prev === 'root') return -1
+        if (curr === 'root') return 1
+        if (curr > prev) return -1
+        if (curr < prev) return 1
+        return 0
+      })
+      .map((name) => ({
+        group: name,
+        commands: groupsLiteral[name].sort((prev, curr) => {
+          if (curr.commandName > prev.commandName) return -1
+          if (curr.commandName < prev.commandName) return 1
+          return 0
+        }),
+      }))
+  }
+}
