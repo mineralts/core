0ff2f70d8b00aff5982f7b621799251ce4ea2140
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-24 13:15:11 +0100

    :construction: Work in progress

---
+++ src/commands/ApplicationDev.ts
@@ -0,0 +1,29 @@
+/*
+ * @mineralts/standalone-preview/MakeCommand.ts
+ *
+ * (c) Parmantier Baptiste
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+import path from 'path'
+import { execSync } from 'child_process'
+import { Command } from '../forge/entities/Command'
+
+export default class ApplicationDev extends Command {
+  public static commandName = 'app:dev'
+  public static description = 'Starting the application in development mode'
+
+  public async run (): Promise<void> {
+    const esbuild = path.join(process.cwd(), 'node_modules', 'esbuild-dev', 'pkg', 'esbuild-dev.bin.js')
+
+    execSync(`node ${esbuild} start/index.ts --commands --watch --supervise`, {
+      cwd: process.cwd(),
+      stdio: 'inherit',
+    })
+
+    process.exit()
+  }
+}
