fd6647b9dbbbc741d5d4f016d57bf8a8df391d0b
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-24 13:01:41 +0100

    :construction: Work in progress

---
+++ src/application/types/index.ts
@@ -0,0 +1,61 @@
+/*
+ * @mineralts/Environment.ts
+ *
+ * (c) Parmantier Baptiste
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+export type ApplicationNode = {
+  major: number
+  minor: number
+  patch: number
+  prerelease: (string | number)[]
+  version: string
+  toString(): string
+}
+
+export interface DirectoriesNode {
+  config: string
+  public: string
+  contracts: string
+  providers: string
+  start: string
+  tests: string
+  [key: string]: string
+}
+
+export type RcFile = {
+  preloads: any[]
+  commands: string[]
+  providers: string[]
+  statics: string[]
+  aliases: {
+    [key: string]: string
+  }
+}
+
+export interface ApplicationContract {
+  container: any
+}
+
+export enum Intent {
+  GUILDS = 1,
+  GUILD_MEMBERS = 2,
+  GUILD_BANS = 4,
+  GUILD_EMOJIS_AND_STICKERS = 8,
+  GUILD_INTEGRATIONS = 16,
+  GUILD_WEBHOOKS = 32,
+  GUILD_INVITES = 64,
+  GUILD_VOICE_STATES = 128,
+  GUILD_PRESENCES = 256,
+  GUILD_MESSAGES = 512,
+  GUILD_MESSAGE_REACTIONS = 1024,
+  GUILD_MESSAGE_TYPING = 2048,
+  DIRECT_MESSAGES = 4096,
+  DIRECT_MESSAGE_REACTIONS = 8192,
+  DIRECT_MESSAGE_TYPING = 16384,
+  ALL = 32767,
+}
