7af36e21228dab28dc49da1eed77055c6ac7c249
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-24 10:55:21 +0100

    :tada: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+/*
+ * packages/index.ts
+ *
+ * (c) Parmantier Baptiste
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+import Kernel from './core/Kernel'
+import { MineralCommand, Subcommand, Command, Option } from './core/entities/Command'
+import { MineralEvent, Event } from './core/entities/Event'
+import { MineralProvider, Provider } from './core/entities/Provider'
+import Ignitor from './core/standalone/Ignitor'
+import MessageEmbed from './api/entities/embed/MessageEmbed'
+
+export * from './typing/interfaces'
+
+export {
+  Kernel,
+  MineralCommand,
+  Command,
+  Subcommand,
+  Option,
+  MineralEvent,
+  Event,
+  Provider,
+  MineralProvider,
+  Ignitor,
+  MessageEmbed
+}
