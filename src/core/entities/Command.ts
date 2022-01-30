4ba65b4ead848211c1c3b4dbd694d20546394208
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-30 20:46:02 +0100

    :construction: Work in progress

--- src/core/entities/Command.ts
+++ src/core/entities/Command.ts
@@ -1,53 +1,69 @@
 import Logger from '@mineralts/logger'
-import { CommandOption, OptionType, Snowflake } from '../../api/types'
+import { CommandOption, CommandOptionType, OptionType, Snowflake } from '../../api/types'
 import { Client } from '../../api/entities'
+import Application from '../../application/Application'
 
-export function Command (name: string, description: string, scope: 'GUILDS' | Snowflake) {
+export function Command (name: string, description: string, scope: 'GUILDS' | Snowflake, isParent?: boolean) {
   return (target: any) => {
     target.identifier = 'slash-command'
+    target.label = name.toLowerCase()
 
-    if (!target.prototype.data) {
-      target.prototype.data = {
-        options: []
-      }
+    const container = Application.getContainer()
+
+    const command = new target() as MineralBaseCommand & { data: any }
+
+    command.logger = Application.getLogger()
+    command.client = Application.getClient()
+    command.parent = isParent || false
+    command.data = {
+      label: name.toLowerCase(),
+      scope: scope,
+      description: description,
+      options: isParent
+        ? target.prototype.subcommands
+        : target.prototype.commandOptions
     }
 
-    target.prototype.data.label = name.toLowerCase()
-    target.prototype.data.scope = scope
-    target.prototype.data.description = description
+    console.log(command.data.options)
+
+    container.commands.set(command.data.label, command)
   }
 }
 
-export function Subcommand (parent: string, name: string, description: string) {
+export function Subcommand (description: string) {
+  return (target, propertyKey: string) => {
+    if (!target.constructor.prototype.subcommands) {
+      target.constructor.prototype['subcommands'] = []
+    }
+
+    target.constructor.prototype.subcommands.push({
+      type: 'SUB_COMMAND',
+      name: propertyKey.toLowerCase(),
+      description,
+      options: target.options
+    })
+  }
+}
+
+export function Option<T extends keyof typeof CommandOptionType | 'CHOICE'> (type: T, options: CommandOption<T>): any {
   return (target: any) => {
-    if (!target.prototype.data) {
-      target.prototype.data = {}
+    if (!target.options) {
+      target['options'] = []
     }
 
-    target.identifier = 'subcommand'
-    target.prototype.data.parent = parent.toLowerCase().split('.')
-    target.prototype.data.label = name.toLowerCase()
-    target.prototype.data.description = description
+    if (!target.prototype.commandOptions) {
+      target.prototype['commandOptions'] = []
+    }
+
+    target.prototype.commandOptions.push({type, ...options})
+    target.options.push({type, ...options})
   }
 }
 
-export function Option<T extends keyof typeof OptionType | 'CHOICE'> (type: T, opt: CommandOption<T>) {
-  return (target: any) => {
-    if (!target.prototype.data) {
-      target.prototype.data = {}
-    }
-
-    if (!target.prototype.data.options) {
-      target.prototype.data.options = []
-    }
-
-    target.prototype.data.options.push({ type, ...opt })
-  }
-}
-
-export abstract class MineralCommand {
+export abstract class MineralBaseCommand {
   public logger!: Logger
   public client!: Client
+  public parent!: boolean
 
   public getLabel!: () => string
   public getDescription!: () => string
@@ -55,6 +71,11 @@ export abstract class MineralCommand {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   public getOption<T extends keyof typeof OptionType | 'CHOICE'> (type: T, name: string): CommandOption<T>
+}
 
+export abstract class MineralCommand extends MineralBaseCommand {
   abstract run (...args: any[]): Promise<void>
+}
+
+export abstract class MineralSubcommand extends MineralBaseCommand {
 }
