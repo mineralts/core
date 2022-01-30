8d83c12435a60655999fdc19e721d695cd563ac6
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-30 23:48:01 +0100

    :construction: Work in progress

--- src/api/entities/interaction/CommandOptions.ts
+++ src/api/entities/interaction/CommandOptions.ts
@@ -16,32 +16,33 @@ export default class CommandOptions {
   }
 
   public getChannel (name: string): ChannelResolvable | undefined {
-    const channel = this.params.options?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: Snowflake }
+    const channel = this.params?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: Snowflake }
     return this.member.guild.channels.cache.get(channel?.value)
   }
 
   public getMember (name: string): GuildMember | undefined {
-    const channel = this.params.options?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: Snowflake }
-    return this.member.guild.channels.cache.get(channel?.value)
+    const channel = this.params?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: Snowflake }
+    return this.member.guild.members.cache.get(channel?.value)
   }
 
   public getString (name: string): string | undefined {
-    const stringValue = this.params.options?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: string }
+    const stringValue = this.params?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: string }
     return stringValue?.value
   }
 
   public getNumber (name: string): number | undefined {
-    const numberValue = this.params.options?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: number }
+    console.log(this.params)
+    const numberValue = this.params?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: number }
     return numberValue?.value
   }
 
   public getBoolean (name: string): boolean | undefined {
-    const numberValue = this.params.options?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: boolean }
+    const numberValue = this.params?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: boolean }
     return numberValue?.value
   }
 
   public getChoices<T> (name: string): T | undefined {
-    const choiceValue = this.params.options?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: T }
+    const choiceValue = this.params?.find((param: CommandParamsResolvable) => param.name == name) as unknown as { value: T }
     return choiceValue?.value
   }
 }
