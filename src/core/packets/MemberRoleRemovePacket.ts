8a73d74c445e5ddfd328bfc6aa68709b6815edc0
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-02-12 16:49:10 +0100

    :construction: Implement MemberRoleRemovePacket

---
+++ src/core/packets/MemberRoleRemovePacket.ts
@@ -0,0 +1,39 @@
+import Assembler from '../../assembler/Assembler'
+import Packet from '../entities/Packet'
+import { Client, Guild, GuildMember, Role, Snowflake } from '../../api/entities'
+import Collection from '../../api/utils/Collection'
+
+export default class MemberRoleRemovePacket extends Packet {
+  public packetType = 'GUILD_MEMBER_UPDATE'
+
+  public async handle (assembler: Assembler, payload: any) {
+    const client: Client = assembler.application.client
+    const guild: Guild | undefined = client.guilds.cache.get(payload.guild_id)
+    const member: GuildMember | undefined = guild?.members.cache.get(payload.user.id)
+
+    if (!member) {
+      assembler.application.logger.error('The member was not found.')
+      return
+    }
+
+    if (payload.roles.length < member.roles.cache.size) {
+      const currentRoles = member.roles.cache.clone()
+      const targetRoles: Collection<Snowflake, Role> = new Collection()
+
+      payload.roles.forEach((id: Snowflake) => {
+        const role = guild?.roles.cache.get(id)
+        if (role) {
+          member.roles.cache.delete(role.id)
+          targetRoles.set(role.id, role)
+        }
+      })
+
+      assembler.eventListener.emit(
+        'guildMemberRoleRemove',
+        member,
+        currentRoles,
+        targetRoles
+      )
+    }
+  }
+}
