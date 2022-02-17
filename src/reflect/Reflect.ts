dd936a02b282f48ec04031246cb4cdf63e37a46f
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-02-17 23:47:35 +0100

    :construction: Work in progress

--- src/reflect/Reflect.ts
+++ src/reflect/Reflect.ts
@@ -1,39 +1,33 @@
-import WebSocket, { Server } from 'ws'
+import WebSocket from 'ws'
 import { DateTime } from 'luxon'
 
 export default class Reflect {
-  public server: Server = new Server({ host: '127.0.0.1', port: 4444 })
-  public clients: WebSocket[] = []
+  public webSocket: WebSocket = new WebSocket('ws://127.0.0.1:4444')
 
   public createClient () {
-    this.server.on('connection', (client: WebSocket) => {
-      this.clients.push(client)
-    })
-
-    this.server.on('close', () => {
-      this.sendNotification('RECONNECT', 'Please reconnect.')
+    this.webSocket.on('open', () => {
+      this.webSocket.send(JSON.stringify({
+        type: 'ACTION',
+        msg: 'COUCOU'
+      }))
     })
   }
 
   public sendEvent (packetType: string, value: any) {
-    this.clients.forEach((client: WebSocket) => {
-      client.send(this.safeStringify({
-        type: 'EVENT',
-        event: packetType,
-        date: DateTime.now().toISO(),
-        payload: value
-      }))
-    })
+    this.webSocket.send(this.safeStringify({
+      type: 'EVENT',
+      event: packetType,
+      date: DateTime.now().toISO(),
+      payload: value
+    }))
   }
 
   public sendNotification (action: string, message: any) {
-    this.clients.forEach((client: WebSocket) => {
-      client.send(JSON.stringify({
-        type: 'NOTIFICATION',
-        action,
-        message,
-      }))
-    })
+    this.webSocket.send(JSON.stringify({
+      type: 'NOTIFICATION',
+      action,
+      message,
+    }))
   }
 
   private safeStringify = (obj, indent = 2) => {
