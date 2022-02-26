4b5c5688084b15da1ea26457a6545c9889e161c0
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-02-26 01:58:19 +0100

    :refactor: Upgrade event names

--- src/core/packets/ChannelDeletePacket.ts
+++ src/core/packets/ChannelDeletePacket.ts
@@ -1,5 +1,11 @@
 import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
+import TextChannel from '../../api/entities/channels/TextChannel'
+import DMChannel from '../../api/entities/channels/DMChannel'
+import CategoryChannel from '../../api/entities/channels/CategoryChannel'
+import VoiceChannel from '../../api/entities/channels/VoiceChannel'
+import StageChannel from '../../api/entities/channels/StageChannel'
+import NewsChannel from '../../api/entities/channels/NewsChannel'
 
 export default class ChannelDeletePacket extends Packet {
   public packetType = 'CHANNEL_DELETE'
@@ -8,12 +14,20 @@ export default class ChannelDeletePacket extends Packet {
     const guild = assembler.application.client.guilds.cache.get(payload.guild_id)
     const channel = guild?.channels.cache.get(payload.id)
 
-    assembler.eventListener.emit('channelDelete', channel)
 
     if (!channel) {
       assembler.application.logger.error('Channel is missing')
       return
     }
+
+    if (channel instanceof TextChannel) assembler.eventListener.emit('delete:TextChannel', channel)
+    if (channel instanceof DMChannel) assembler.eventListener.emit('delete:DmChannel', channel)
+    if (channel instanceof CategoryChannel) assembler.eventListener.emit('delete:CategoryChannel', channel)
+    if (channel instanceof VoiceChannel) assembler.eventListener.emit('delete:VoiceChannel', channel)
+    if (channel instanceof StageChannel) assembler.eventListener.emit('delete:StageChannel', channel)
+    if (channel instanceof NewsChannel) assembler.eventListener.emit('delete:NewsChannel', channel)
+
+    assembler.eventListener.emit('delete:Channel', channel)
 
     guild?.channels.cache.delete(channel.id)
   }
