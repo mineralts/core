6ae6dcdd3a09d8e6b6903cb1754bd4a6c9a870dd
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-02-26 02:02:43 +0100

    :refactor: Upgrade event names

--- src/assembler/builders/ReactionBuilder.ts
+++ src/assembler/builders/ReactionBuilder.ts
@@ -1,4 +1,7 @@
-import { Client, Emoji, Reaction, GuildMember } from '../../api/entities'
+import GuildMember from '../../api/entities/guild/GuildMember'
+import Emoji from '../../api/entities/emoji'
+import Client from '../../api/entities/client'
+import Reaction from '../../api/entities/reaction/Reaction'
 
 export default class ReactionBuilder {
   constructor (private client: Client, private readonly emoji: Emoji, private member: GuildMember) {
