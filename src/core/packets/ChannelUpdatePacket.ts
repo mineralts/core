3467b071b4d1325d3c5c8e7738b5a334fec65bbc
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-02-26 01:58:23 +0100

    :refactor: Upgrade event names

--- src/core/packets/ChannelUpdatePacket.ts
+++ src/core/packets/ChannelUpdatePacket.ts
@@ -1,7 +1,14 @@
 import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import { ChannelBuilder } from '../../assembler/builders'
-import { MessageManager, TextChannelResolvable } from '../../api/entities'
+import TextChannelResolvable from '../../api/entities/channels/TextChannelResolvable'
+import MessageManager from '../../api/entities/message/MessageManager'
+import TextChannel from '../../api/entities/channels/TextChannel'
+import DMChannel from '../../api/entities/channels/DMChannel'
+import CategoryChannel from '../../api/entities/channels/CategoryChannel'
+import VoiceChannel from '../../api/entities/channels/VoiceChannel'
+import StageChannel from '../../api/entities/channels/StageChannel'
+import NewsChannel from '../../api/entities/channels/NewsChannel'
 
 export default class ChannelUpdatePacket extends Packet {
   public packetType = 'CHANNEL_UPDATE'
@@ -19,7 +26,14 @@ export default class ChannelUpdatePacket extends Packet {
       after.guild = guild as any
     }
 
-    assembler.eventListener.emit('channelUpdate', before!, after)
+    if (before instanceof TextChannel) assembler.eventListener.emit('update:TextChannel', before, after)
+    if (before instanceof DMChannel) assembler.eventListener.emit('update:DmChannel', before, after)
+    if (before instanceof CategoryChannel) assembler.eventListener.emit('update:CategoryChannel', before, after)
+    if (before instanceof VoiceChannel) assembler.eventListener.emit('update:VoiceChannel', before, after)
+    if (before instanceof StageChannel) assembler.eventListener.emit('update:StageChannel', before, after)
+    if (before instanceof NewsChannel) assembler.eventListener.emit('update:NewsChannel', before, after)
+
+    assembler.eventListener.emit('update:Channel', before, after)
 
     guild?.channels.cache.set(after.id, after as any)
   }
