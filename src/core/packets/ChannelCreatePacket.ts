8e5d3e5a64290d1720546571686da451288a2cd0
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-02-26 01:58:16 +0100

    :refactor: Upgrade event names

--- src/core/packets/ChannelCreatePacket.ts
+++ src/core/packets/ChannelCreatePacket.ts
@@ -1,8 +1,14 @@
 import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import { ChannelBuilder } from '../../assembler/builders'
-import { MessageManager, TextChannelResolvable } from '../../api/entities'
 import DMChannel from '../../api/entities/channels/DMChannel'
+import StageChannel from '../../api/entities/channels/StageChannel'
+import NewsChannel from '../../api/entities/channels/NewsChannel'
+import TextChannelResolvable from '../../api/entities/channels/TextChannelResolvable'
+import MessageManager from '../../api/entities/message/MessageManager'
+import TextChannel from '../../api/entities/channels/TextChannel'
+import CategoryChannel from '../../api/entities/channels/CategoryChannel'
+import VoiceChannel from '../../api/entities/channels/VoiceChannel'
 
 export default class ChannelCreatePacket extends Packet {
   public packetType = 'CHANNEL_CREATE'
@@ -25,6 +31,13 @@ export default class ChannelCreatePacket extends Packet {
       assembler.application.client.privates.cache.set(channel.id, channel)
     }
 
-    assembler.eventListener.emit('channelCreate', channel)
+    if (channel instanceof TextChannel) assembler.eventListener.emit('create:TextChannel', channel)
+    if (channel instanceof DMChannel) assembler.eventListener.emit('create:DmChannel', channel)
+    if (channel instanceof CategoryChannel) assembler.eventListener.emit('create:CategoryChannel', channel)
+    if (channel instanceof VoiceChannel) assembler.eventListener.emit('create:VoiceChannel', channel)
+    if (channel instanceof StageChannel) assembler.eventListener.emit('create:StageChannel', channel)
+    if (channel instanceof NewsChannel) assembler.eventListener.emit('create:NewsChannel', channel)
+
+    assembler.eventListener.emit('create:channel', channel)
   }
 }
