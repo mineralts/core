6702839a802a13b8965e3d0672070a7033988132
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:20:31 +0100

    :construction: Convert to IOC

---
+++ src/core/services/MineralEventService.ts
@@ -0,0 +1,34 @@
+import Collection from '../../api/utils/Collection'
+import { MineralEvent } from '../entities/Event'
+import Application from '../../application/Application'
+import { Client } from '../../typing/interfaces'
+
+export default class MineralEventService {
+  public collection: Collection<string, Collection<string, MineralEvent>> = new Collection()
+
+  public register (path, item: { new (): MineralEvent, event: string }) {
+    const logger = Application.singleton().resolveBinding('Mineral/Core/Logger')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+
+    const event = new item() as MineralEvent & { event: string }
+    event.logger = logger!
+    event.client = client as unknown as Client
+
+    const eventContainer = this.collection.get(item.event)
+
+    if (!eventContainer) {
+      const events: Collection<string, MineralEvent> = new Collection()
+      this.collection.set(
+        item.event,
+        events.set(path, event)
+      )
+    } else {
+      eventContainer.set(path, event)
+    }
+
+    emitter.on(item.event, async (...args: any[]) => {
+      await event.run(...args)
+    })
+  }
+}
