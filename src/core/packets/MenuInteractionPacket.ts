afcd6feef0b5eaa795d959fbe1c8899c6569f8d6
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:19:37 +0100

    :construction: Convert to IOC

--- src/core/packets/MenuInteractionPacket.ts
+++ src/core/packets/MenuInteractionPacket.ts
@@ -1,26 +1,27 @@
 import Packet from '../entities/Packet'
-import Assembler from '../../assembler/Assembler'
 import { CommandType, InteractionType } from '../../api/types'
 import MenuInteractionBuilder from '../../assembler/builders/MenuInteractionBuilder'
+import Application from '../../application/Application'
 
 export default class MenuInteractionPacket extends Packet {
   public packetType = 'INTERACTION_CREATE'
 
-  public async handle (assembler: Assembler, payload: any): Promise<void> {
+  public async handle (payload: any): Promise<void> {
     if (payload.type !== InteractionType.APPLICATION_COMMAND || payload.data.type === CommandType.CHAT_INPUT) {
       return
     }
 
-    const client = assembler.application.client
-    const container = assembler.application.container
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+    const menus = Application.singleton().resolveBinding('Mineral/Core/SelectMenus')
 
-    const guild = client.guilds.cache.get(payload.guild_id)
+    const guild = client?.guilds.cache.get(payload.guild_id)
     const member = guild?.members.cache.get(payload.member.user.id)
 
-    const interactionBuilder = new MenuInteractionBuilder(assembler.application.client, member!)
+    const interactionBuilder = new MenuInteractionBuilder(client!, member!)
     const interaction = interactionBuilder.build(payload)
 
-    const menu = container.menus.get(interaction.params.name)
+    const menu = menus.get(interaction.params.name)
 
     if (!menu) {
       return
@@ -28,6 +29,6 @@ export default class MenuInteractionPacket extends Packet {
 
     await menu.run(interaction as any)
 
-    assembler.eventListener.emit('menuInteraction', interaction)
+    emitter.emit('menuInteraction', interaction)
   }
 }
