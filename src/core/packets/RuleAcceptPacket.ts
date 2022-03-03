43ad01a6a54fc976508219b4f9dc7cd00756cf95
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 22:00:36 +0100

    :construction: Convert to IOC

--- src/core/packets/RuleAcceptPacket.ts
+++ src/core/packets/RuleAcceptPacket.ts
@@ -1,14 +1,15 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import GuildMember from '../../api/entities/guild/GuildMember'
 import Guild from '../../api/entities/guild/Guild'
+import Application from '../../application/Application'
 
 export default class RuleAcceptPacket extends Packet {
   public packetType = 'GUILD_MEMBER_UPDATE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const client = assembler.application.client
-    const guild: Guild | undefined = client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+    const guild: Guild | undefined = client?.guilds.cache.get(payload.guild_id)
     const member: GuildMember | undefined = guild?.members.cache.get(payload.user.id)
 
     if (!member) {
@@ -17,7 +18,7 @@ export default class RuleAcceptPacket extends Packet {
 
     if (member.isPending() !== payload.pending) {
       member.pending = false
-      assembler.eventListener.emit('accept:Rules', client)
+      emitter.emit('accept:Rules', client)
     }
   }
 }
