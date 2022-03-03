cb0d3985b9e3f69fac21117eaad466e14403b759
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 22:01:16 +0100

    :construction: Convert to IOC

--- src/core/packets/VoiceMuteRemovePacket.ts
+++ src/core/packets/VoiceMuteRemovePacket.ts
@@ -1,17 +1,18 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import GuildMember from '../../api/entities/guild/GuildMember'
+import Application from '../../application/Application'
 
 export default class VoiceMuteRemovePacket extends Packet {
   public packetType = 'VOICE_STATE_UPDATE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const client = assembler.application.client
-    const guild = client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+    const guild = client?.guilds.cache.get(payload.guild_id)
     const member: GuildMember | undefined = guild?.members.cache.get(payload.member.user.id)
 
     if (member?.voice.channelId && member?.voice.mute === true && payload.mute === false) {
-      assembler.eventListener.emit('remove:MemberMute', member)
+      emitter.emit('remove:MemberMute', member)
     }
   }
 }
