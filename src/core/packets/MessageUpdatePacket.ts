2458b181ad0492823936a439850bfd729ae183f2
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:20:08 +0100

    :construction: Convert to IOC

--- src/core/packets/MessageUpdatePacket.ts
+++ src/core/packets/MessageUpdatePacket.ts
@@ -1,21 +1,24 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import { MessageBuilder } from '../../assembler/builders'
 import TextChannel from '../../api/entities/channels/TextChannel'
+import Application from '../../application/Application'
 
 export default class MessageUpdatePacket extends Packet {
   public packetType = 'MESSAGE_UPDATE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const guild = assembler.application.client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+
+    const guild = client?.guilds.cache.get(payload.guild_id)
     const channel = guild?.channels.cache.get(payload.channel_id) as TextChannel
     const before = channel.messages.cache.get(payload.id)
 
-    const messageBuilder = new MessageBuilder(assembler.application.client as any)
+    const messageBuilder = new MessageBuilder(client!)
     const after = messageBuilder.build(payload)
 
 
-    assembler.eventListener.emit('update:Message', before || undefined, after)
+    emitter.emit('update:Message', before || undefined, after)
 
     channel.messages.cache.set(after!.id, after!)
   }
