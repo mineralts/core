d70eee9f7899555bfd82c664b1abb8b7c753dc58
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 22:01:23 +0100

    :construction: Convert to IOC

--- src/core/packets/VoiceStateUpdatePacket.ts
+++ src/core/packets/VoiceStateUpdatePacket.ts
@@ -1,25 +1,26 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import VoiceChannel from '../../api/entities/channels/VoiceChannel'
 import GuildMember from '../../api/entities/guild/GuildMember'
 import { VoiceStateBuilder } from '../../assembler/builders'
 import VoiceState from '../../api/entities/voice/VoiceState'
+import Application from '../../application/Application'
 
 export default class VoiceStateUpdatePacket extends Packet {
   public packetType = 'VOICE_STATE_UPDATE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const client = assembler.application.client
-    const guild = client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+    const guild = client?.guilds.cache.get(payload.guild_id)
     const before = guild?.members.cache.get(payload.member.user.id)
 
     const voiceChannel: VoiceChannel | undefined = guild?.channels.cache.get(payload.channel_id)
     const member: GuildMember | undefined = guild?.members.cache.get(payload.member.user.id)
 
-    const voiceStateBuilder: VoiceStateBuilder = new VoiceStateBuilder(client as any, guild!, member!, voiceChannel!)
+    const voiceStateBuilder: VoiceStateBuilder = new VoiceStateBuilder(client!, guild!, member!, voiceChannel!)
     const voiceState: VoiceState = voiceStateBuilder.build(payload)
 
-    assembler.eventListener.emit('update:VoiceState', before, voiceState)
+    emitter.emit('update:VoiceState', before, voiceState)
 
     member!.voice = voiceState
   }
