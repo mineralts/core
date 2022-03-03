866f2db45f847f292e8fe2c8cab460614d8ff1d3
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:18:55 +0100

    :construction: Convert to IOC

--- src/core/packets/MemberLeavePacket.ts
+++ src/core/packets/MemberLeavePacket.ts
@@ -1,18 +1,19 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import GuildMember from '../../api/entities/guild/GuildMember'
 import Guild from '../../api/entities/guild/Guild'
+import Application from '../../application/Application'
 
 export default class MemberLeavePacket extends Packet {
   public packetType = 'GUILD_MEMBER_REMOVE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const client = assembler.application.client
-    const guild: Guild | undefined = client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
 
+    const guild: Guild | undefined = client?.guilds.cache.get(payload.guild_id)
     const guildMember: GuildMember | undefined = guild?.members.cache.get(payload.user.id) || guild?.bots.cache.get(payload.user.id)
 
-    assembler.eventListener.emit('leave:Member', guildMember)
+    emitter.emit('leave:Member', guildMember)
 
     if (guildMember!.user.isBot()) {
       guild?.bots.cache.delete(guildMember!.id)
