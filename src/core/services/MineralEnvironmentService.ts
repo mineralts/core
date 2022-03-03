2fb4c07dbf1a526840cf12578a8e509a16564a6e
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 01:07:06 +0100

    :construction: Convert to IOC

--- src/core/services/MineralEnvironmentService.ts
+++ src/core/services/MineralEnvironmentService.ts
@@ -3,6 +3,7 @@ import fs from 'fs'
 import path from 'path'
 import YAML from 'js-yaml'
 import { RcFile } from '../types'
+import { Intent } from '../../application/types'
 
 interface Environment {
   appName: string,
@@ -11,16 +12,23 @@ interface Environment {
   debug: boolean
   reflect: boolean
   rcFile: RcFile
+  mode: 'production' | 'testing' | 'development'
+  intents: {
+    selected: 'ALL' | Exclude<keyof typeof Intent, 'ALL'>[]
+    bitfield: number
+  }
 }
+
+type EnvironmentType<T> = T extends keyof Environment ? Environment[T] : any
 
 export class MineralEnvironmentService {
   public collection: Collection<keyof Environment | string, unknown> = new Collection()
 
-  public registerKey<T> (key: string, value: T) {
+  public registerKey<T extends keyof Environment | string> (key: T | string, value: EnvironmentType<T>): void {
     this.collection.set(key, value)
   }
 
-  public resolveKey<T extends keyof Environment | string> (key: T | string): (T extends keyof Environment ? Environment[T] : any) | undefined {
+  public resolveKey<T extends keyof Environment | string> (key: T | string): EnvironmentType<T> | undefined {
     return this.collection.get(key)
   }
 
