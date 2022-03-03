928ad16bc9589820703c2c7aaf2ccb0f409d9323
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:14:06 +0100

    :construction: Convert to IOC

--- src/core/packets/EmojiCreatePacket.ts
+++ src/core/packets/EmojiCreatePacket.ts
@@ -1,16 +1,19 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import { Snowflake } from '../../api/types'
 import { EmojiBuilder } from '../../assembler/builders'
 import Collection from '../../api/utils/Collection'
 import Emoji from '../../api/entities/emoji'
 import Guild from '../../api/entities/guild/Guild'
+import Application from '../../application/Application'
 
 export default class EmojiCreatePacket extends Packet {
   public packetType = 'GUILD_EMOJIS_UPDATE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const guild: Guild | undefined = assembler.application.client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+
+    const guild: Guild | undefined = client?.guilds.cache.get(payload.guild_id)
     const guildEmojis: Collection<Snowflake, Emoji> = guild?.emojis.cache.clone()
     const payloadEmojis: any[] = payload.emojis
 
@@ -26,7 +29,7 @@ export default class EmojiCreatePacket extends Packet {
 
       guild?.emojis.cache.set(emoji.id, emoji)
 
-      assembler.eventListener.emit('create:Emoji', emoji)
+      emitter.emit('create:Emoji', emoji)
     }
   }
 }
