8955bba96c8c8abed84ab6fecac130d17ef249df
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:16:40 +0100

    :construction: Convert to IOC

--- src/core/packets/GuildDeletePacket.ts
+++ src/core/packets/GuildDeletePacket.ts
@@ -1,14 +1,15 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
+import Application from '../../application/Application'
 
 export default class GuildDeletePacket extends Packet {
   public packetType = 'GUILD_DELETE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const client = assembler.application.client
-    const guild = client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+    const guild = client?.guilds.cache.get(payload.guild_id)
 
-    assembler.eventListener.emit('delete:Guild', guild)
-    client.guilds.cache.delete(payload.guild_id)
+    emitter.emit('delete:Guild', guild)
+    client?.guilds.cache.delete(payload.guild_id)
   }
 }
