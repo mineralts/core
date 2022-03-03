dd68f4bcc9b37c2147d63e2193dd65c00d78776e
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:20:44 +0100

    :construction: Convert to IOC

--- src/core/packets/ModalInteractionPacket.ts
+++ src/core/packets/ModalInteractionPacket.ts
@@ -1,24 +1,26 @@
 import Packet from '../entities/Packet'
-import Assembler from '../../assembler/Assembler'
 import { InteractionType } from '../../api/types'
 import ModalInteractionBuilder from '../../assembler/builders/ModalInteractionBuilder'
+import Application from '../../application/Application'
 
 export default class ModalInteractionPacket extends Packet {
   public packetType = 'INTERACTION_CREATE'
 
-  public async handle (assembler: Assembler, payload: any): Promise<void> {
+  public async handle (payload: any): Promise<void> {
     if (payload.type !== InteractionType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE) {
       return
     }
 
-    const client = assembler.application.client
-    const guild = client.guilds.cache.get(payload.guild_id)
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+
+    const guild = client?.guilds.cache.get(payload.guild_id)
     const member = guild?.members.cache.get(payload.member.user.id)
 
-    const interactionBuilder = new ModalInteractionBuilder(assembler.application.client, member!)
+    const interactionBuilder = new ModalInteractionBuilder(client!, member!)
     const interaction = interactionBuilder.build(payload)
 
-    assembler.eventListener.emit('interactionModalCreate', interaction)
-    assembler.eventListener.emit(`interactionModal::${interaction.customId}`, interaction)
+    emitter.emit('interactionModalCreate', interaction)
+    emitter.emit(`interactionModal::${interaction.customId}`, interaction)
   }
 }
