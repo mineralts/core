dd04b822f7965962f35253a3cfb3a4aee808de60
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:13:15 +0100

    :construction: Convert to IOC

--- src/core/packets/ChannelDeletePacket.ts
+++ src/core/packets/ChannelDeletePacket.ts
@@ -1,4 +1,3 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import TextChannel from '../../api/entities/channels/TextChannel'
 import DMChannel from '../../api/entities/channels/DMChannel'
@@ -6,27 +5,32 @@ import CategoryChannel from '../../api/entities/channels/CategoryChannel'
 import VoiceChannel from '../../api/entities/channels/VoiceChannel'
 import StageChannel from '../../api/entities/channels/StageChannel'
 import NewsChannel from '../../api/entities/channels/NewsChannel'
+import Application from '../../application/Application'
 
 export default class ChannelDeletePacket extends Packet {
   public packetType = 'CHANNEL_DELETE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const guild = assembler.application.client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+    const logger = Application.singleton().resolveBinding('Mineral/Core/Logger')
+
+    const guild = client?.guilds.cache.get(payload.guild_id)
     const channel = guild?.channels.cache.get(payload.id)
 
     if (!channel) {
-      assembler.application.logger.error('Channel is missing')
+      logger?.error('Channel is missing')
       return
     }
 
-    if (channel instanceof TextChannel) assembler.eventListener.emit('delete:TextChannel', channel)
-    if (channel instanceof DMChannel) assembler.eventListener.emit('delete:DmChannel', channel)
-    if (channel instanceof CategoryChannel) assembler.eventListener.emit('delete:CategoryChannel', channel)
-    if (channel instanceof VoiceChannel) assembler.eventListener.emit('delete:VoiceChannel', channel)
-    if (channel instanceof StageChannel) assembler.eventListener.emit('delete:StageChannel', channel)
-    if (channel instanceof NewsChannel) assembler.eventListener.emit('delete:NewsChannel', channel)
+    if (channel instanceof TextChannel) emitter.emit('delete:TextChannel', channel)
+    if (channel instanceof DMChannel) emitter.emit('delete:DmChannel', channel)
+    if (channel instanceof CategoryChannel) emitter.emit('delete:CategoryChannel', channel)
+    if (channel instanceof VoiceChannel) emitter.emit('delete:VoiceChannel', channel)
+    if (channel instanceof StageChannel) emitter.emit('delete:StageChannel', channel)
+    if (channel instanceof NewsChannel) emitter.emit('delete:NewsChannel', channel)
 
-    assembler.eventListener.emit('delete:Channel', channel)
+    emitter.emit('delete:Channel', channel)
 
     guild?.channels.cache.delete(channel.id)
   }
