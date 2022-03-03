98f85df608dc00f9f218970935fbde9e099eac06
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:14:14 +0100

    :construction: Convert to IOC

--- src/core/packets/EmojiDeletePacket.ts
+++ src/core/packets/EmojiDeletePacket.ts
@@ -1,15 +1,18 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import { Snowflake } from '../../api/types'
 import Collection from '../../api/utils/Collection'
 import Guild from '../../api/entities/guild/Guild'
 import Emoji from '../../api/entities/emoji'
+import Application from '../../application/Application'
 
 export default class EmojiDeletePacket extends Packet {
   public packetType = 'GUILD_EMOJIS_UPDATE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const guild: Guild | undefined = assembler.application.client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+
+    const guild: Guild | undefined = client?.guilds.cache.get(payload.guild_id)
     const guildEmojis: Collection<Snowflake, Emoji> = guild?.emojis.cache.clone()
 
     if (payload.emojis.length < guildEmojis!.size) {
@@ -20,7 +23,7 @@ export default class EmojiDeletePacket extends Packet {
         }
       })
 
-      assembler.eventListener.emit('delete:Emoji', guildEmojis.first())
+      emitter.emit('delete:Emoji', guildEmojis.first())
       guild?.emojis.cache.delete(guildEmojis.first()!.id)
     }
   }
