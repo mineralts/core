54a474b7c603242330d07bdb28ec379f9ae73ecb
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:11:43 +0100

    :construction: Convert to IOC

--- src/commands/ApplicationDev.ts
+++ src/commands/ApplicationDev.ts
@@ -16,12 +16,19 @@ export default class ApplicationDev extends Command {
   public static commandName = 'app:dev'
   public static description = 'Starting the application in development mode'
 
+  public static settings = {
+    loadApp: true
+  }
+
   public async run (): Promise<void> {
     const esbuild = path.join(process.cwd(), 'node_modules', 'esbuild-dev', 'pkg', 'esbuild-dev.bin.js')
 
-    execSync(`node ${esbuild} start/index.ts --commands --watch --supervise`, {
+    execSync(`node ${esbuild} start/index.ts --watch --supervise`, {
       cwd: process.cwd(),
       stdio: 'inherit',
+      env: {
+        NODE_ENV: 'development'
+      }
     })
 
     process.exit()
