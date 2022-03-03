b38d90c2d01f703932fd842196fcd1e4e2f65cb4
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 22:00:53 +0100

    :construction: Convert to IOC

--- src/core/packets/TypingStartPacket.ts
+++ src/core/packets/TypingStartPacket.ts
@@ -1,20 +1,22 @@
-
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import TextChannelResolvable from '../../api/entities/channels/TextChannelResolvable'
 import Guild from '../../api/entities/guild/Guild'
 import GuildMember from '../../api/entities/guild/GuildMember'
+import Application from '../../application/Application'
 
 export default class TypingStartPacket extends Packet {
   public packetType = 'TYPING_START'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const guild: Guild | undefined = assembler.application.client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+
+    const guild: Guild | undefined = client?.guilds.cache.get(payload.guild_id)
     const channel: TextChannelResolvable | undefined = guild?.channels.cache.get(payload.channel_id)
     const member: GuildMember | undefined = guild?.members.cache.get(payload.user_id)
 
     if (member) {
-      assembler.eventListener.emit('start:typing', member!, channel)
+      emitter.emit('start:typing', member!, channel)
     }
   }
 }
