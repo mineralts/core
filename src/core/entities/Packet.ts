359065a3d33c3e45840deb15aa76033dbcc0094d
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:20:59 +0100

    :construction: Convert to IOC

--- src/core/entities/Packet.ts
+++ src/core/entities/Packet.ts
@@ -1,6 +1,4 @@
-import Assembler from '../../assembler/Assembler'
-
 export default abstract class Packet {
   public packetType!: string
-  public abstract handle (assembler: Assembler, payload: any): Promise<void>
+  public abstract handle (payload: any): Promise<void>
 }
