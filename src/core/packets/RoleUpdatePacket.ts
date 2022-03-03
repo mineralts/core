7257310a63ace5fdfe3be07ca025db71388c0416
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 22:00:33 +0100

    :construction: Convert to IOC

--- src/core/packets/RoleUpdatePacket.ts
+++ src/core/packets/RoleUpdatePacket.ts
@@ -1,20 +1,23 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import { RoleBuilder } from '../../assembler/builders'
 import Guild from '../../api/entities/guild/Guild'
 import Role from '../../api/entities/roles'
+import Application from '../../application/Application'
 
 export default class RoleUpdatePacket extends Packet {
   public packetType = 'GUILD_ROLE_UPDATE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const guild: Guild | undefined = assembler.application.client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+
+    const guild: Guild | undefined = client?.guilds.cache.get(payload.guild_id)
     const before: Role | undefined = guild?.roles.cache.get(payload.role.id)
 
     const roleBuilder: RoleBuilder = new RoleBuilder()
     const after: Role = roleBuilder.build(payload.role)
 
-    assembler.eventListener.emit('update:Role', before, after)
+    emitter.emit('update:Role', before, after)
 
     guild?.roles.cache.set(after.id, after)
   }
