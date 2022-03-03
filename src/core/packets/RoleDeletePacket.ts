0c7a7ee5a7fe6d0a6d94531cb12b0d262446ce96
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 22:00:27 +0100

    :construction: Convert to IOC

--- src/core/packets/RoleDeletePacket.ts
+++ src/core/packets/RoleDeletePacket.ts
@@ -1,20 +1,23 @@
-import Assembler from '../../assembler/Assembler'
 import Packet from '../entities/Packet'
 import Guild from '../../api/entities/guild/Guild'
 import Role from '../../api/entities/roles'
+import Application from '../../application/Application'
 
 export default class RoleDeletePacket extends Packet {
   public packetType = 'GUILD_ROLE_DELETE'
 
-  public async handle (assembler: Assembler, payload: any) {
-    const guild: Guild | undefined = assembler.application.client.guilds.cache.get(payload.guild_id)
+  public async handle (payload: any) {
+    const emitter = Application.singleton().resolveBinding('Mineral/Core/Emitter')
+    const client = Application.singleton().resolveBinding('Mineral/Core/Client')
+
+    const guild: Guild | undefined = client?.guilds.cache.get(payload.guild_id)
     const role: Role | undefined = guild?.roles.cache.get(payload.role_id)
 
     if (!role) {
       return
     }
 
-    assembler.eventListener.emit('delete:Role', role)
+    emitter.emit('delete:Role', role)
 
     guild?.roles.cache.delete(role.id)
   }
