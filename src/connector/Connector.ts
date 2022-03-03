904232a1eb227d657ecd188dbb2c20c66da502a0
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:13:56 +0100

    :construction: Convert to IOC

--- src/connector/Connector.ts
+++ src/connector/Connector.ts
@@ -1,14 +1,13 @@
 import Http from './http'
 import WebSocketManager from './managers/WebSocketManager'
-import EventsListener from '../assembler/EventsListener'
 import Application from '../application/Application'
 
 export default class Connector {
   public http: Http
   public websocketManager: WebSocketManager
 
-  constructor (private application: Application, private eventEmitter: EventsListener) {
-    this.http = new Http(eventEmitter)
+  constructor (private application: Application) {
+    this.http = new Http()
     this.websocketManager = new WebSocketManager(application, this.http, { shards: 'auto' })
   }
 }
