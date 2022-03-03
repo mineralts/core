f45cd39fcbb2fd28f94c7e23b8ac5156ecb6e0eb
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-03-03 21:20:27 +0100

    :construction: Convert to IOC

--- src/core/services/MineralEnvironmentService.ts
+++ src/core/services/MineralEnvironmentService.ts
@@ -4,15 +4,18 @@ import path from 'path'
 import YAML from 'js-yaml'
 import { RcFile } from '../types'
 import { Intent } from '../../application/types'
+import Application from '../../application/Application'
 
 interface Environment {
   appName: string,
   appVersion: string
+  token: string
   root: string
   debug: boolean
   reflect: boolean
   rcFile: RcFile
   mode: 'production' | 'testing' | 'development'
+  mineralDependencies: { [K: string]: unknown }[]
   intents: {
     selected: 'ALL' | Exclude<keyof typeof Intent, 'ALL'>[]
     bitfield: number
@@ -21,25 +24,26 @@ interface Environment {
 
 type EnvironmentType<T> = T extends keyof Environment ? Environment[T] : any
 
-export class MineralEnvironmentService {
+export default class MineralEnvironmentService {
   public collection: Collection<keyof Environment | string, unknown> = new Collection()
 
-  public registerKey<T extends keyof Environment | string> (key: T | string, value: EnvironmentType<T>): void {
+  public registerKey<T extends keyof Environment | string> (key: T, value: EnvironmentType<T>): void {
     this.collection.set(key, value)
   }
 
-  public resolveKey<T extends keyof Environment | string> (key: T | string): EnvironmentType<T> | undefined {
+  public resolveKey<T extends keyof Environment | string> (key: T): EnvironmentType<T> | undefined {
     return this.collection.get(key)
   }
 
   public resolveEnvironment () {
-    const root = this.resolveKey<string>('root')!
+    const helpers = Application.singleton().resolveBinding('Mineral/Core/Helpers')
+    const root = this.resolveKey('root')!
 
     const environmentContent = fs.readFileSync(path.join(root, 'env.yaml'), 'utf-8')
     const environment = YAML.load(environmentContent) as object
 
     Object.entries(environment).forEach(([key, value]: [string, unknown]) => {
-      this.registerKey(key, value)
+      this.registerKey(helpers.camelCase(key), value)
     })
   }
 }
